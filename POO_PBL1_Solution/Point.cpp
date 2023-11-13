07f23e7b7222a6d08e8104aaec8bb921b0ae3621
Florentms <florent.maillardsalin@viacesi.fr>
2023-11-13 10:27:20 +0100

    Version final avec déplacement

--- POO_PBL1_Solution/Point.cpp
+++ POO_PBL1_Solution/Point.cpp
@@ -16,9 +16,29 @@ Point::Point(float a, float b)
 	y = b;
 }
 
+float Point::convertIntToFloat(int a)
+{
+	return (float)a;
+}
+
+Point::Point(int a, int b)
+{
+	Point(convertIntToFloat(a), convertIntToFloat(b));
+}
+
+void Point::setx(int a)
+{
+	setx(convertIntToFloat(a));
+}
+
 void Point::setx(float a)
 {
 	x = a;
+}
+
+void Point::sety(int a)
+{
+	sety(convertIntToFloat(a));
 }
 
 void Point::sety(float b)
@@ -39,6 +59,12 @@ float Point::gety(void)
 void Point::printPoint(void)
 {
 	std::cout << "x : " << x << "  y : " << y << std::endl;
+}
+
+void Point::move(float a, float b)
+{
+	x = x + a;
+	y = y + b;
 }
 
 
--- POO_PBL1_Solution/Point.h
+++ POO_PBL1_Solution/Point.h
@@ -6,16 +6,27 @@ private :
 	float y;
 
 public :
+	//Constructor
 	Point(void);
 	Point(float, float);
-	//point(int, int);
-	//void setx(int);
+	Point(int, int);
+
+	//Convert methode
+	float convertIntToFloat(int);
+	
+	//Mutateur 
+	void setx(int);
 	void setx(float);
-	//void sety(int);
+	void sety(int);
 	void sety(float);
+
+	//Accesseur
 	float getx(void);
 	float gety(void);
+
+	//Print
 	void printPoint(void);
+
 	//void moveLeft(void);
 	//void moveDroite(void);
 	//void moveTop(void);
@@ -24,7 +35,7 @@ public :
 	//void moveDroite(int);
 	//void moveTop(int);
 	//void moveBottom(int);
-	//void move(float,float);
+	void move(float,float);
 
 };
 
